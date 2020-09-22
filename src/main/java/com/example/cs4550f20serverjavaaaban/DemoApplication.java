eb3b8b1c743e19d2dbdd0c51c32fcf4ba273b8da
Zain <reply2zain@gmail.com>
2020-09-22 09:58:09 -0400

    first commit

---
+++ src/main/java/com/example/cs4550f20serverjavaaaban/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.cs4550f20serverjavaaaban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
